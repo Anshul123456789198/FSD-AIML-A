3ee160c6b71665c983a59f335e2e69019878400f
Anshul <rajputanshul0708@gmail.com>
2025-09-18 09:25:50 +0530

    final

---
+++ HTML/a.js
@@ -0,0 +1,31 @@
+// console.log("Hello World")
+// a=3
+// console.log(a);
+// {
+// let b=50;
+
+// console.log(b);
+// console.log(a);
+// }
+
+// console.log(a);
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log("sum=" +sum(12,30));
+
+// function sub(a,b){
+//     return a-b;
+// }
+// console.log(sub(50,30));
+// let a=4;
+// console.log(console.log(a));
+
+
+function register(){
+    console.log("register sucessfully")
+}
+register();
